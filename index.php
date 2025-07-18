20b007c415f07e82d430c4b0b26d4e3c70d7a176
M Rizky Nugraha <rizkynugraha1121@gmail.com>
2025-07-18 14:54:09 +0700

    Perubahan terbaru

--- index.php
+++ index.php
@@ -10,10 +10,30 @@
   <h2>Form Absensi Karyawan</h2>
   <form action="simpan_absen.php" method="POST">
     <label>Nama:</label>
-    <input type="text" name="nama" required>
+    <select name="nama" required>
+      <option value="Alvin Fitra">Alvin Fitra</option>
+      <option value="Yogie Fatwa Gofur">Yogie Fatwa Gofur</option>
+      <option value="Dayat Hidayat">Dayat Hidayat</option>
+      <option value="Anggara Ariffin">Anggara Ariffin</option>
+      <option value="Roni Ramdani">Roni Ramdani</option>
+      <option value="Enhari Satibie">Enhari Satibie</option>
+      <option value="Gin Ginanjar">Gin Ginanjar</option>
+      <option value="Adi Pratama">Adi Pratama</option>
+      <option value="Indra Hidayat">Indra Hidayat</option>
+</select>
     
     <label>Jabatan:</label>
-    <input type="text" name="jabatan" required>
+    <select name="jabatan" required>
+      <option value="Direktur sales">Direktur Sales</option>
+      <option value="Direktur">Direktur</option>
+      <option value="Direktur Teknik">Direktur Teknik</option>
+      <option value="Manager Finance">Manager Finance</option>
+      <option value="Sales Manager">Sales Manager</option>
+      <option value="Manager logistik">Manager Logistik</option>
+      <option value="SPV Sales">SPV Sales</option>
+      <option value="Admin">Admin</option>
+      <option value="Komisaris">Komisaris</option>
+</select>
 
     <label>Status:</label>
     <select name="status" required>
